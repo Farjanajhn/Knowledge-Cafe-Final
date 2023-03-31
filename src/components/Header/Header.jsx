f4f6c2237d9f01f900c821c4cfe1776e0106d2b7
Farjana <farjanajhn12@gmail.com>
2023-03-31 13:57:36 +0200

    added header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <nav className='header'>
+    <h3>Knowledge cafe</h3>
+    <div>
+  
+      <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt=""/>
+      </div>
+  </nav>
+  );
+};
+
+export default Header;
